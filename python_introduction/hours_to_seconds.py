7a281868e3ce283fb062f9e080659fc49a4c6267
Rita Wanjiku <mwangirita@gmail.com>
2024-12-15 22:58:10 +0300

    Python script that converts a specific number of hours into seconds

---
+++ python_introduction/hours_to_seconds.py
@@ -0,0 +1,8 @@
+# Defining the variable hours and assigning it a value
+hours = 2 # The value represents number of hours
+
+# Calculate the number of seconds in the given hours
+seconds = hours * 3600
+
+# Print the result in the required formart
+print(f"{ours} hours(s) is {seconds} seconds.")
